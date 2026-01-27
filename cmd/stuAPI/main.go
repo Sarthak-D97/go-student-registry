5fa3d15e56ce58099a35eb07fda1f5f2ce892635
Darkshadow-ssh <gsarthak913@gmail.com>
2026-01-27 16:57:15 +0530

    server error handling

--- cmd/stuAPI/main.go
+++ cmd/stuAPI/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Sarthak-D97/go_stuAPI/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, Student API!")
+	// fmt.Println("Hello, Student API!")
+	//load configuration
+	cfg := config.MustLoad()
+
+	//database setup
+	//router setup
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Student API"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	slog.Info("Starting server", "address", cfg.HTTPServer.Addr)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %s", err.Error())
+		}
+	}()
+
+	<-done
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shut down server", "error", err)
+	}
+
+	slog.Info("Server exited properly")
 }
